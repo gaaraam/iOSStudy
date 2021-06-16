e49a9861560a9f36ce886e8d7e02dc912e903ab3
LeeGaram <star146913@gmail.com>
2021-06-16 17:06:21 +0900

    UnwindSegue Created

---
+++ Scene-UnwindSegue/Scene-UnwindSegue/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Scene-UnwindSegue
+//
+//  Created by 이가람 on 2021/06/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func unwindToVC(_ segue: UIStoryboardSegue){
+        
+    }
+
+}
+
