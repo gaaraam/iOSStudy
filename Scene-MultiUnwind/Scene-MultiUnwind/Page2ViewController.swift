454fd8d82e62c91dd0363c00e6805173f01b96fb
LeeGaram <star146913@gmail.com>
2021-06-16 17:22:54 +0900

    MuitiUnwind Created

---
+++ Scene-MultiUnwind/Scene-MultiUnwind/Page2ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  Page2ViewController.swift
+//  Scene-MultiUnwind
+//
+//  Created by 이가람 on 2021/06/16.
+//
+
+import UIKit
+
+class Page2ViewController: UIViewController{
+    
+    @IBAction func gotoPage2(_segue: UIStoryboardSegue){
+        
+    }
+}
